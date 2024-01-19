c9b71d40437288d280b74c673120aa97d377f554
Mahmoud Tarek <62097408+Ma7moudAlgamal@users.noreply.github.com>
2024-01-19 12:35:50 +0300

    made the brackets and changed my pfp

--- main.js
+++ main.js
@@ -4,3 +4,9 @@ fetch("tables.html")
     const tableContainer = document.getElementById("tables");
     tableContainer.innerHTML = content;
   });
+// fetch("brackets.html")
+//   .then((response) => response.text())
+//   .then((content) => {
+//     const tableContainer = document.getElementById("brackets");
+//     tableContainer.innerHTML = content;
+//   });
