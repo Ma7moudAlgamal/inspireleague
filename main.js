95c341df0de23fa28a8386c91cf3a78424d0be40
Mahmoud Tarek <62097408+Ma7moudAlgamal@users.noreply.github.com>
2024-02-01 11:10:02 +0300

    Added the brackets

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-fetch("tables.html")
-  .then((response) => response.text())
-  .then((content) => {
-    const tableContainer = document.getElementById("tables");
-    tableContainer.innerHTML = content;
-  });
-// fetch("brackets.html")
+// fetch("tables.html")
 //   .then((response) => response.text())
 //   .then((content) => {
-//     const tableContainer = document.getElementById("brackets");
+//     const tableContainer = document.getElementById("tables");
 //     tableContainer.innerHTML = content;
 //   });
+fetch("brackets.html")
+  .then((response) => response.text())
+  .then((content) => {
+    const tableContainer = document.getElementById("brackets");
+    tableContainer.innerHTML = content;
+  });
